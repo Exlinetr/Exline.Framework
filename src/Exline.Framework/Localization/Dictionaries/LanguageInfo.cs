db8ca051264477243412927b308956a4027aa21f
Muhammet Furkan DOĞAN <dogan.furkan@msn.com>
2018-04-23 13:37:45 +0300

    language info oluşturuldu.

--- src/Exline.Framework/Localization/Dictionaries/LanguageInfo.cs
+++ src/Exline.Framework/Localization/Dictionaries/LanguageInfo.cs
@@ -7,6 +7,7 @@ namespace Exline.Framework.Localization.Dictionaries
         public string Name { get; set; }
         public string DisplayName { get; set; }
         public string Icon { get; set; }
+        public string Acronym { get; set; }
         public bool IsRigthToLeft
         {
             get
@@ -24,10 +25,11 @@ namespace Exline.Framework.Localization.Dictionaries
         public bool IsDefault { get; set; }
         public bool IsDisabled { get; set; }
 
-        public LanguageInfo(string name,string displayName,string icon=null,bool isDefault=false,bool isDisabled=false)
+        public LanguageInfo(string name,string displayName,string acronym,string icon=null,bool isDefault=false,bool isDisabled=false)
         {
             this.Name=name;
             this.DisplayName=displayName;
+            this.Acronym=acronym;
             this.Icon=icon;
             this.IsDefault=isDefault;
             this.IsDisabled=isDisabled;
